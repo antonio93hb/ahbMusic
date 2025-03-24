683bda406cab8b21bdb4c49e7992ca9d6848e830
antonio93hb <antonio93hb@hotmail.com>
2025-03-24 12:51:55 +0100

    Init project finished

--- app/src/main/java/com/example/ahbmusic/ReproductorActivity.java
+++ app/src/main/java/com/example/ahbmusic/ReproductorActivity.java
@@ -1,6 +1,9 @@
 package com.example.ahbmusic;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ReproductorActivity extends AppCompatActivity {
+    protected TextView textView1;
+    protected TextView textView2;
+    protected TextView textView3;
+    protected ImageButton buttonPause;
+    protected ImageButton buttonPlay;
+    protected ImageButton buttonStop;
+    protected Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,13 @@ public class ReproductorActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView1 = (TextView) findViewById(R.id.textView1_reproductor);
+        textView2 = (TextView) findViewById(R.id.textView2_reproductor);
+        textView3 = (TextView) findViewById(R.id.textView3_reproductor);
+        buttonPause = (ImageButton) findViewById(R.id.buttonPause_reproductor);
+        buttonPlay = (ImageButton) findViewById(R.id.buttonPlay_reproductor);
+        buttonStop = (ImageButton) findViewById(R.id.buttonStop_reproductor);
+        button1 = (Button) findViewById(R.id.buttonVolver_reproductor);
     }
 }
